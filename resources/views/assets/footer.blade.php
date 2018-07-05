f27463726b023242d301a1c917a2aef4d2248baa
Unknown <lucaspellegrinowd@gmail.com>
2018-07-05 14:01:37 +0200

    style fixes

--- resources/views/assets/footer.blade.php
+++ resources/views/assets/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer fixed-bottom primary-c-bg p-4 text-white ">
     <div class="row ">
         <div class="col-md-4">
-           Copyright 2018 - Nom de la boite
+           <small>GI - ORT - CSI</small>
         </div>
     </div>
 </footer>
--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -8,40 +8,36 @@
 				<br><br><br>
 				<div class="mx-1 px-1 display-tickets">
 
-					<div class="card" style="">
+					<div class="card p-4" style="">
 						<div class="card-body">
 							<h5 class="card-title">{{$ticket->objet}}</h5>
+							<hr>
 							<h6 class="card-subtitle mb-2 text-muted">Posté par: {{$ticket->user->name}}</h6>
 							<p class="card-text">
-                                <span class="user-field">Statut:
-                                  <small>{{$ticket->status}}</small>
-                                </span><br>
+								<span class="user-field">Statut:
+									<small>{{$ticket->status}}</small>
+								</span><br>
 								<span class="user-field">Priorité:
-                                  <small>{{$ticket->priority}}</small>
-                                </span><br>
+									<small>{{$ticket->priority}}</small>
+								</span><br>
 								<span class="user-field">Catégorie:
-                                    <small>{{$ticket->category->name}}</small>
-                                </span><br><br>
+									<small>{{$ticket->category->name}}</small>
+								</span><br>
+								<hr>
 								<span class="user-field">Description:
-                                <hr>
-                                  <span class="ticket-description">
-                                    {{$ticket->description}}
-                                  </span>
-                                </span>
+									<br>
+									<span class="ticket-description">
+										{{$ticket->description}}
+									</span>
+								</span>
 							</p>
-
-
 							<br>
 							<div class="d-flex row">
-
-								<a class="btn col-6 btn-primary" href="{{route('tickets.edit', ['ticket' => $ticket])}}" role="button" >Editer</a>
+								<a class="btn col-6  btn-primary" href="{{route('tickets.edit', ['ticket' => $ticket])}}" role="button" >Editer</a>
 								<a class="btn col-6  btn-warning" href="{{route('tickets.index', ['ticket' => $ticket])}}" role="button" >Retour</a>
-
 							</div>
-
 						</div>
 					</div>
-
 					<br><br>
 				</div>
 			</div>
